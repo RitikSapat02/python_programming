64f5c0c2b1463b0708dd6e7257df95626bc24020
Ritik Sapat <118529770+RitikSapat02@users.noreply.github.com>
2022-12-09 19:43:11 +0530

    Delete Others directory

--- Others/01_walrus_operator.py
+++
@@ -1,9 +0,0 @@
-# a = 'hellooooooooooo'
-# if(len(a)>10):                             normally
-#     print(f"too long {len(a)} elements")
-
-
-
-a = 'hellooooooooooo'          
-if((n:=len(a))>10):                                                 
-    print(f"too long {n} elements") 
--- Others/03_args_kwargs.py
+++
@@ -1,7 +0,0 @@
-def super_fun(a,b,*args,c=10,d=0,**kwargs):
-    whole_sum = sum(args)+a+b+c+d
-    for x in kwargs.values():
-        whole_sum+=x;
-    return whole_sum
-
-print(super_fun(2,3,3,4,5,5,p=2,r=7))
